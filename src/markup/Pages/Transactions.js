efc6440d54d5c639dabff1e37e95331d57de34dc
ParasharM007 <parasharmohit421@gmail.com>
2024-11-20 18:20:54 +0530

    ppt changed

--- src/markup/Pages/Transactions.js
+++ src/markup/Pages/Transactions.js
@@ -19,7 +19,7 @@ import pic9 from "../../assests/1 (9).png";
 import pic10 from "../../assests/1 (10).png";
 import pic11 from "../../assests/1 (11).png";
 import pic12 from "../../assests/1 (12).png";
-import pic13 from "../../assests/1 (13).png";
+// import pic13 from "../../assests/1 (13).png";
 import { Carousel } from "react-bootstrap";
 import "./aboutus.css";
 import { useLocation } from 'react-router-dom';
